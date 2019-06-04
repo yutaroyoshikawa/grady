3b5d451cb33a4899da8ba1de90eb04219def7ce4
Cherry0202 <ex00ia2432@gmail.com>
2019-06-04 11:01:52 +0900

    [update]input.stories

--- stories/input.stories.js
+++ stories/input.stories.js
@@ -1,10 +1,17 @@
+import { storiesOf } from '@storybook/vue'
 import AdultInput from '../components/inputs/adultInput.vue'
-import { storiesOf } from '@storybook/vue';
+import EmailInput from '../components/inputs/emailInput.vue'
 
-storiesOf('Input', module)
-  .add('adultInput', () => ({
-    components: { AdultInput },
-    render(h) {
-      return <AdultInput onchange={this.action} />
-    }
-  }))
+storiesOf('Input', module).add('adultInput', () => ({
+  components: { AdultInput },
+  render(h) {
+    return <AdultInput onchange={this.action} />
+  }
+}))
+
+storiesOf('Input', module).add('emailInput', () => ({
+  components: { EmailInput },
+  render(h) {
+    return <EmailInput />
+  }
+}))
