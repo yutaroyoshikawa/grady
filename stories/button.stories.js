b07a1cbd656080abb0bf9b65a34dbc3e43bf4534
Miccili <wot.dannbo@gmail.com>
2019-06-04 19:30:21 +0900

    [UPDATE] storybookに追加

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,4 +1,5 @@
 import CloseButton from '../components/buttons/closeButton.vue'
+import BackButton from '../components/buttons/backButton.vue'
 import { storiesOf } from '@storybook/vue';
 
 storiesOf('Button', module)
@@ -6,5 +7,13 @@ storiesOf('Button', module)
     components: { CloseButton },
     render(h) {
       return <CloseButton />
+    }
+  }))
+
+storiesOf('Button', module)
+  .add('backButton', () => ({
+    components: { BackButton },
+    render(h) {
+      return <BackButton />
     }
   }))
