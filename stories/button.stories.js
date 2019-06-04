6dd77d73de8a2262a7ec046ea597f734d6b78dcd
Miccili <wot.dannbo@gmail.com>
2019-06-04 09:38:45 +0900

    [FIX] next修正、storybook追加

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,4 +1,5 @@
 import CloseButton from '../components/buttons/closeButton.vue'
+import NextButton from '../components/buttons/nextButton.vue'
 import { storiesOf } from '@storybook/vue';
 
 storiesOf('Button', module)
@@ -6,5 +7,13 @@ storiesOf('Button', module)
     components: { CloseButton },
     render(h) {
       return <CloseButton />
+    }
+  }))
+
+storiesOf('Button',module)
+  .add('nextButton', () => ({
+    components: { NextButton },
+    render(h) {
+      return <NextButton />
     }
   }))
