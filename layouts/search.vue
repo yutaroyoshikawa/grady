82ce141db07f69868a87ace010c82fd798ba69de
Cherry0202 <ex00ia2432@gmail.com>
2019-06-06 12:56:18 +0900

    [update]layouts/search

--- layouts/search.vue
+++ layouts/search.vue
@@ -1,28 +1,53 @@
 <template>
-  <body>
-    <input type="search" class="search" />
-  </body>
+  <form class="wrapper">
+    <input type="search" class="search_box" />
+    <div class="search_button">
+      <font-awesome-icon icon="search" size="3x" style="color: white" />
+    </div>
+  </form>
 </template>
 
-<script>
-export default {
-  name: 'Search'
-}
+<script lang="ts">
+import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSearch)
+
+export default Vue.extend({
+  components: {
+    'font-awesome-icon': FontAwesomeIcon
+  }
+})
 </script>
 
 <style scoped>
 input {
   appearance: none;
   resize: none;
-  padding: 0;
+  padding-right: 90px;
+  padding-left: 20px;
   border: 0;
   outline: none;
   background: transparent;
+  font-size: xx-large;
+  color: white;
 }
-.search {
+.wrapper {
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 722px;
+}
+.search_box {
   width: 722px;
   height: 66px;
   border-radius: 66px;
-  border: solid #000000;
+  border: solid #ffffff;
+}
+.search_button {
+  position: absolute;
+  right: 35px;
 }
 </style>
