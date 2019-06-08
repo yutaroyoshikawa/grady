8a831525f5f53f3a6c2585e1d1eeae0cb3d908b1
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-06-08 13:11:15 +0900

    [ADD]showingMarkの作成

---
+++ components/marks/showingMark.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="wrap">
+      <span>上映中</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  span {
+    display: flex;
+    width: 132px;
+    height: 49px;
+    background-color: #860B52;
+    color: #FFF;
+    font-size: 22px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 19px 5px 5px 5px;
+  }
+}
+</style>
--- stories/mark.stories.js
+++ stories/mark.stories.js
@@ -1,3 +1,10 @@
 import { storiesOf } from '@storybook/vue';
+import ShowingMark from '../components/marks/showingMark.vue'
 
 storiesOf('Mark', module)
+  .add('showingMark', () => ({
+    components: { ShowingMark },
+    render(h) {
+      return <ShowingMark />
+    }
+  }))
