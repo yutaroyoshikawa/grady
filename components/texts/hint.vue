ed476c0f2f6633f51a52ba850f2a874ffddadef9
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-06-09 22:58:02 +0900

    [ADD]hintの作成

---
+++ components/texts/hint.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <div class="wrap">
+      <div>
+        <label
+          :class="{
+          active: content,
+          inactive: !content
+        }"
+        >{{ number }}</label>
+      </div>
+      <p v-if="content" class="active-content">{{ content }}</p>
+      <p v-if="!content" class="inactive-content">Comming soon...</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    content: String,
+    number: Number
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  align-items: center;
+
+  label {
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #0d0d36;
+  }
+
+  p {
+    display: inline-block;
+    word-wrap: break-word;
+  }
+
+  .active {
+    background-color: #fff;
+  }
+
+  .inactive {
+    background-color: #72729d;
+  }
+
+  .active-content {
+    color: #fff;
+  }
+
+  .inactive-content {
+    color: #72729d;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .wrap {
+    label {
+      width: 68px;
+      height: 68px;
+      font-size: 44px;
+    }
+
+    p {
+      font-size: 44px;
+      width: 361px;
+      margin: 0 0 0 48px;
+    }
+  }
+}
+
+@media screen and (min-width: 768px) and (max-width: 1024px) {
+  .wrap {
+    label {
+      width: 58px;
+      height: 58px;
+      font-size: 37px;
+    }
+
+    p {
+      font-size: 37px;
+      width: 411px;
+      margin: 0 0 0 37px;
+    }
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .wrap {
+    label {
+      width: 36px;
+      height: 36px;
+      font-size: 21px;
+    }
+
+    p {
+      font-size: 21px;
+      width: 238px;
+      margin: 0 0 0 20px;
+    }
+  }
+}
+</style>
--- stories/text.stories.js
+++ stories/text.stories.js
@@ -1,3 +1,31 @@
+import Hint from '../components/texts/hint.vue';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/vue';
 
 storiesOf('Text', module)
+  .addDecorator(
+    withKnobs
+  )
+  .addParameters({
+    backgrounds: [
+      { name: 'default', value: '#0D0D36', default: true },
+      { name: 'facebook', value: '#3b5998' },
+    ],
+  })
+  .add('hint', () => ({
+    components: { Hint },
+    props: {
+      number: {
+        default: number('number', 1, {
+          min: 1,
+          max: 3
+        })
+      },
+      content: {
+        default: text('content', 'hogehoge')
+      }
+    },
+    render(h) {
+      return <Hint number={this.number} content={this.content} />
+    }
+  }))
