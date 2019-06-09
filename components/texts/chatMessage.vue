4841a3bb281757bc95085339b1de93b31710ac63
Cherry0202 <ex00ia2432@gmail.com>
2019-06-09 19:55:53 +0900

    [save]chat

---
+++ components/texts/chatMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="chat_box">
+    <div class="contents">hoeuheouhoueheouaheafafafaf</div>
+    <div class="contents">hoeuheouhoueheoua</div>
+    <div class="contents">hoeuheouhoueheouah</div>
+    <div class="contents">hoeuheouhoueheouah</div>
+    <div class="contents">hoeuheouhoueheouah</div>
+
+    <div class="contents">hoeuheouhoueheouah</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ChatMessage'
+}
+</script>
+
+<style scoped>
+.chat_box {
+  display: flex;
+  align-items: center;
+  width: 291px;
+  min-height: 61px;
+  border: solid #000000 1px;
+  background-color: #ffffff;
+  border-radius: 111px;
+  flex-wrap: wrap;
+}
+.contents {
+  text-align: center;
+  padding: 10px;
+  width: 291px;
+  margin: 10px;
+}
+</style>
--- stories/text.stories.js
+++ stories/text.stories.js
@@ -1,3 +1,12 @@
-import { storiesOf } from '@storybook/vue';
+import { storiesOf } from '@storybook/vue'
+import { withKnobs } from '@storybook/addon-knobs'
+import ChatMessage from '../components/texts/chatMessage'
 
 storiesOf('Text', module)
+  .addDecorator(withKnobs)
+  .add('chatMessage', () => ({
+    components: { ChatMessage },
+    render(h) {
+      return <ChatMessage />
+    }
+  }))
