d04ca3048c83bf005460841d73d5cfbe1ed9fa2f
Miccili <wot.dannbo@gmail.com>
2019-06-12 16:45:25 +0900

    [FIX] 名前の変更とストーリーズ

---
+++ layouts/secretCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <router-link to="{ 'value' }">
+    <div class="secretGanreButton">
+      <showing-mark class="mark" />
+      <div class="tagText">
+        {{ text }}
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ShowingMark from '../components/marks/showingMark.vue'
+
+export default Vue.extend({
+  props: {
+    text: String,
+    value: String
+  },
+  components: {
+    'showing-mark': ShowingMark,
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.secretGanreButton {
+  background: #000000;
+  height: 348px;
+  width: 242px;
+  position: relative;
+  border-radius: 20px;
+  .mark {
+    position: absolute;
+  }
+  .tagText {
+    display: flex;
+    color: #000000;
+    font-size: 32px;
+    justify-content: center;
+    align-items: center;
+    height: 63px;
+    width: 242px;
+    background: #ffffff;
+    position: absolute;
+	  bottom: 50px;
+  }
+}
+</style>
+
--- stories/layout.stories.js
+++ stories/layout.stories.js
@@ -1,6 +1,7 @@
 import ScreeningStatusBar from '../layouts/screeningStatusBar.vue'
 import MovieThumbnail from '../layouts/movieThumbnail.vue'
 import Stepper from '../layouts/stepper.vue'
+import SecretCard from '../layouts/secretCard.vue'
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/vue';
 
@@ -41,5 +42,11 @@ storiesOf('Layout', module)
     components: { Stepper },
     render(h) {
       return <Stepper />
+    }
+  }))
+  .add('secretCard', () => ({
+    components: { SecretCard },
+    render(h) {
+      return <SecretCard />
     }
   }))
