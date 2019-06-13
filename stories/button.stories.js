0ebeb8b748ece311572d0f26b3ab80a6e17bac0a
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-06-13 10:32:09 +0900

    [FIX]コンフリクト解消

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,9 +1,5 @@
 import CloseButton from '../components/buttons/closeButton.vue'
-<<<<<<< HEAD
 import NextButton from '../components/buttons/nextButton.vue'
-=======
-import PaymentButton from '../components/buttons/paymentButton.vue'
->>>>>>> 658436841f03de32af9659183a501631adf21b69
 import GotoWathcButton from '../components/buttons/goToWatchButton.vue'
 import BackButton from '../components/buttons/backButton.vue'
 import TagButton from '../components/buttons/tagButton.vue'
@@ -30,24 +26,10 @@ storiesOf('Button',module)
       return <CloseButton />
     }
   }))
-<<<<<<< HEAD
   .add('nextButton', () => ({
     components: { NextButton },
     render(h) {
       return <NextButton />
-=======
-storiesOf('Button', module)
-  .add('closeButton', () => ({
-    components: { CloseButton },
-    render(h) {
-      return <CloseButton />
-    }
-  }))
-  .add('paymentButton', () => ({
-    components: { PaymentButton },
-    render(h) {
-      return <PaymentButton />
->>>>>>> 658436841f03de32af9659183a501631adf21b69
     }
   }))
   .add('goToWatchButton', () => ({
