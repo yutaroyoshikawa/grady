e636080e61827fc64eb4276a70b8278c4d8d28ad
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-06-14 12:10:53 +0900

    [FIX]flamelink修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,30 @@
-import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
+import * as flamelink from 'flamelink/app'
+import * as adminConfig from './grady-43e4a-firebase-adminsdk-94m3w-16c816eec7.json'
+// tslint:disable-next-line: no-import-side-effect
+import 'flamelink/content'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert(adminConfig as any),
+  databaseURL: 'https://grady-43e4a.firebaseio.com',
+})
+
+const app = (flamelink as any)({
+  firebaseApp,
+  env: 'production', // optional, defaults to `production`
+  locale: 'en-US', // optional, defaults to `en-US`
+  dbType: 'cf', // optional, defaults to `rtdb` - can be 'rtdb' or 'cf' (Real-time DB vs Cloud Firestore)
+  isAdminApp: true
+})
+
+export const helloWorld = functions.region('asia-northeast1').https.onRequest((request, response) => {
+  app.content.get({
+    schemaKey: 'movieInfo',
+  })
+    .then((res:any) => {
+      console.log(res)
+      response.send(res)
+    })
+    .catch((e:any) => console.log(`エラー:${e}`))
+});
