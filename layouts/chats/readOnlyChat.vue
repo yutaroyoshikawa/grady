f4e391c378eb43412ffc07b16e96317f0b5164c6
Cherry0202 <ex00ia2432@gmail.com>
2019-06-14 12:36:40 +0900

    [UPDATE]css作成

--- layouts/chats/readOnlyChat.vue
+++ layouts/chats/readOnlyChat.vue
@@ -1,13 +1,33 @@
 <template>
-    $END$
+  <div class="chat_box">
+    <p class="contents"></p>
+  </div>
 </template>
 
-<script>
-  export default {
-    name: 'readOnlyChat'
-  }
+<script lang="ts">
+export default {
+  name: 'readOnlyChat'
+}
 </script>
 
 <style scoped>
-
+.chat_box {
+  display: flex;
+  align-items: center;
+  width: 291px;
+  min-height: 61px;
+  height: auto !important;
+  border: solid #000000 1px;
+  background-color: #ffffff;
+  border-radius: 111px;
+  flex-wrap: wrap;
+  text-align: center;
+  word-wrap: break-word;
+}
+p.contents {
+  padding: 10px;
+  width: 291px;
+  margin: 30px;
+  word-break: break-all;
+}
 </style>
