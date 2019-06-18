041740aa464a2b6ac05f92653d411e173bea4cde
Miccili <wot.dannbo@gmail.com>
2019-06-18 10:23:04 +0900

    [FIX]CSSとかを変更

---
+++ components/selector/creditCardMonth.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="wrap">
+      <select name="cardExpirationMonth" class="monthData">
+        <option value="default" disabled selected>月</option>
+        <option value="1">01</option>
+        <option value="2">02</option>
+        <option value="3">03</option>
+        <option value="4">04</option>
+        <option value="5">05</option>
+        <option value="6">06</option>
+        <option value="7">07</option>
+        <option value="8">08</option>
+        <option value="9">09</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  width: 60px;
+  border-bottom: solid 2px #fff;
+  transition: all 200ms ease;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.2);
+  }
+
+  select {
+    appearance: none;
+    border: none;
+    outline: none;
+    background: transparent;
+    width: 100%;
+    font-size: 29px;
+    border: none;
+    padding: 0 10px;
+    transition: all 300ms ease;
+    background: transparent;
+    color: #fff;
+  }
+}
+</style>
