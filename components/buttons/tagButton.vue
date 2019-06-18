2b4c3d2f436afaf7f02bd6e07fe69fe7a5adf166
Miccili <wot.dannbo@gmail.com>
2019-06-18 11:11:08 +0900

    [UPDATE]レスポンシブ化

--- components/buttons/tagButton.vue
+++ components/buttons/tagButton.vue
@@ -38,21 +38,46 @@ button {
   padding: 0;
   appearance: none;
 }
-.tagButton {
-  font-size: 17px;
-  height: 29px;
-  width: 114px;
-  border-radius: 29px;
-  border: 1px solid #ffffff;
-  color: #ffffff;
-  &:active {
-    background: #ffffff;
-    color: #000000;
-  }
+
+.tagButton:active {
+  background: #ffffff;
+  color: #000000;
 }
+
 .active {
   color: #000;
   background: #fff;
   border-color: #000;
+}
+
+@media screen and (min-width: 1024px) {
+  .tagButton {
+    font-size: 17px;
+    height: 29px;
+    width: 114px;
+    border-radius: 29px;
+    border: 1px solid #ffffff;
+    color: #ffffff;
+  }
+}
+
+@media screen and (min-width: 768px) and (max-width: 1024px) {
+  .tagButton {
+    font-size: 17px;
+    height: 29px;
+    width: 114px;
+    border-radius: 29px;
+    border: 1px solid #ffffff;
+    color: #ffffff;
+  }
+}
+
+@media screen and (min-width: 300px) {
+  .tagButton {
+    width: 84.83px;
+    height: 21.58px;
+    font-size: 12px;
+    border-radius: 61px; 
+  }
 }
 </style>
