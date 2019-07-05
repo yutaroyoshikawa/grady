eadb45103c76b09c4f83724b860cf40bb4bb16a7
Miccili <wot.dannbo@gmail.com>
2019-07-05 14:56:45 +0900

    [ADD]hintButton作成、レスポンシブ対応

---
+++ components/buttons/hintButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button>
+    <font-awesome-icon class="hint-button" icon="info-circle" />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add({ faInfoCircle })
+Vue.component('info-circle', FontAwesomeIcon)
+export default Vue.extend({})
+</script>
+
+<style lang="scss">
+button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+  appearance: none;
+}
+
+.hint-button {
+  color: #fff;
+}
+
+@media screen and (max-width: 1024px) {
+  .hint-button {
+    font-size: 40.43px;
+  }
+}
+
+@media screen and (min-width: 767px) and (max-width: 1024px) {
+  .hint-button {
+    font-size: 40.43px;
+  }
+}
+</style>
