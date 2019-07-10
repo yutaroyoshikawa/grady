f10cdb24474931ece60b38e2f1bc166b8fda1047
Cherry0202 <ex00ia2432@gmail.com>
2019-07-10 14:36:32 +0900

    [UPDATE]middleware/secret

--- middleware/secret.ts
+++ middleware/secret.ts
@@ -2,7 +2,7 @@ export default (
 function (route: { genre: string; params: any; }) {
     // log出力
     const check = route.params.genre
-    if (check == 'Action' || check == 'Sucpense' || check == 'Science Fiction' ||check == 'Horror' || check == 'Comedy' || check == 'Romance') {
+    if (check == 'Action' || check == 'Suspense' || check == 'Science Fiction' ||check == 'Horror' || check == 'Comedy' || check == 'Romance') {
       return
     } else if (check == null) {
       return
