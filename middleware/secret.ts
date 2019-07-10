67ae82f14166110aa46e5db30b85de4c0414b649
Cherry0202 <ex00ia2432@gmail.com>
2019-07-10 12:22:00 +0900

    [UPDATE]secret

--- middleware/secret.ts
+++ middleware/secret.ts
@@ -3,7 +3,9 @@ function (route: { genre: string; params: any; }) {
     // log出力
     const hoge = route.params.genre
     if (hoge == 'Action' || hoge == 'Sucpense' || hoge == 'Science Fiction' ||hoge == 'Horror' || hoge == 'Comedy' || hoge == 'Romance') {
-      window.alert('ok')
+      return
+    } else if (hoge == null) {
+      return
     } else {
       window.alert('不正アクセスです')
     }
