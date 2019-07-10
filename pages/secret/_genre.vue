2c483819cb3bc4b77af1030a00c38a9804e5413c
Cherry0202 <ex00ia2432@gmail.com>
2019-07-10 10:35:22 +0900

    [UPDATE]_genre

--- pages/secret/_genre.vue
+++ pages/secret/_genre.vue
@@ -84,4 +84,82 @@ export default Vue.extend({
   }
 })
 </script>
+<style scoped lang="scss">
+.wrapper {
+  display: grid;
+  grid-template-columns: 181px 640px 263px 638px 199px;
+  grid-template-rows: 237px 737px 106px;
+  background-color: #0a2e41;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
 
+  .top {
+    grid-column-start: 1;
+    grid-column-end: 6;
+  }
+
+  .main {
+    .drawer-btn {
+      margin-top: 370px;
+    }
+
+    .secret-text {
+      color: #ffffff;
+      font-size: 80px;
+    }
+
+    .chats-description {
+      /*width: 638px;*/
+      width: 100%;
+      height: 100%;
+      color: #ffffff;
+      /*background-color: lightcyan;*/
+
+      .chats-title {
+        font-size: 38px;
+      }
+      .chats-massage {
+        font-size: 21px;
+        border-bottom: solid 3px #ffffff;
+      }
+
+      .hoge {
+        /*background-color: lightblue;*/
+        display: flex;
+        justify-content: center;
+        height: 100%;
+      }
+    }
+  }
+
+  .drawer-enter-active,
+  .drawer-leave-active {
+    transition: transform 400ms ease;
+  }
+
+  .drawer {
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 20;
+  }
+
+  .drawer-enter {
+    transform: translateX(100%);
+  }
+
+  .drawer-enter-to {
+    transform: translateX(0);
+  }
+
+  .drawer-leave-to {
+    transform: translateX(100%);
+  }
+
+  .footer {
+    grid-column-start: 1;
+    grid-column-end: 6;
+  }
+}
+</style>
