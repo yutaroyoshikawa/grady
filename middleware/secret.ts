9c332d0198a25c086533029bd6a328208d5a6860
Cherry0202 <ex00ia2432@gmail.com>
2019-07-10 12:12:43 +0900

    [UPDATE]secret

--- middleware/secret.ts
+++ middleware/secret.ts
@@ -5,6 +5,6 @@ function (route: { genre: string; params: any; }) {
     if (hoge == 'Action' || hoge == 'Sucpense' || hoge == 'Science Fiction' ||hoge == 'Horror' || hoge == 'Comedy' || hoge == 'Romance') {
       window.alert('ok')
     } else {
-      window.alert('hogehoge')
+      window.alert('不正アクセスです')
     }
 })
