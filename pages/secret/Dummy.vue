4b9accd10d855500915620986ff9b7f01ccfb242
Cherry0202 <ex00ia2432@gmail.com>
2019-07-11 09:38:56 +0900

    [UPDATE]chatMessageInput

---
+++ pages/secret/Dummy.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="wrap">
+      <chat-message-input />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ChatMessageInput from '~/components/inputs/chatMessageInput'
+
+export default Vue.extend({
+  components: {
+    'chat-message-input': ChatMessageInput
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  background-color: #0a2e41;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
+</style>
