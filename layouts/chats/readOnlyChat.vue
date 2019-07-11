f29785d47c666e4ce3f6fa7a6afb281b1da28dc4
Miccili <wot.dannbo@gmail.com>
2019-07-11 10:50:32 +0900

    [UPDATE] メディアクエリー

--- layouts/chats/readOnlyChat.vue
+++ layouts/chats/readOnlyChat.vue
@@ -3,14 +3,10 @@
     <div class="wrapper">
       <div v-for="(chat, index) in chats" :key="index">
         <div v-if="index % 2 === 0" class="chat_box">
-          <p class="contents">
-            {{ chat.contents }}
-          </p>
+          <p class="contents">{{ chat.contents }}</p>
         </div>
         <div v-else class="chat_box2">
-          <p class="contents">
-            {{ chat.contents }}
-          </p>
+          <p class="contents">{{ chat.contents }}</p>
         </div>
       </div>
     </div>
@@ -35,40 +31,78 @@ export default Vue.extend({
 <style scoped lang="scss">
 .chat_box {
   display: flex;
-  /*align-items: center;*/
-  width: 291px;
-  min-height: 61px;
   height: auto !important;
   border: solid #000000 1px;
   background-color: #ffffff;
-  border-radius: 56px;
   flex-wrap: wrap;
-  /*text-align: center;*/
   word-wrap: break-word;
-  margin-top: 60px;
-  margin-bottom: 60px;
 }
 .chat_box2 {
   display: flex;
   margin: 0 0 0 auto;
-  /*align-items: center;*/
-  width: 291px;
-  min-height: 61px;
   height: auto !important;
   border: solid #000000 1px;
   background-color: #ffffff;
-  border-radius: 56px;
   flex-wrap: wrap;
-  /*text-align: center;*/
   word-wrap: break-word;
 }
 p.contents {
-  padding: 10px;
-  width: 291px;
-  margin: 30px;
   word-break: break-all;
 }
-.wrapper {
-  width: 532px;
+
+@media screen and (min-width: 1024px) {
+  .chat_box {
+    width: 316px;
+    min-height: 111px;
+    border-radius: 56px;
+    margin-top: 60px;
+    margin-bottom: 60px;
+  }
+  .chat_box2 {
+    width: 316px;
+    min-height: 111px;
+    border-radius: 56px;
+  }
+  p.contents {
+    padding: 10px;
+    margin: 20px;
+  }
+}
+
+@media screen and (min-width: 767px) and (max-width: 1024px) {
+  .chat_box {
+    width: 152.87px;
+    min-height: 53.7px;
+    border-radius: 50px;
+    margin-top: 22.27px;
+    margin-bottom: 22.27px;
+  }
+  .chat_box2 {
+    width: 152.87px;
+    min-height: 53.7px;
+    border-radius: 50px;
+  }
+  p.contents {
+    padding: 10px;
+    margin: 20px;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .chat_box {
+    width: 103.8px;
+    min-height: 26.63px;
+    border-radius: 26.63px;
+    margin-top: 11.27px;
+    margin-bottom: 11.27px;
+  }
+  .chat_box2 {
+    width: 103.8px;
+    min-height: 26.63px;
+    border-radius: 26.63px;
+  }
+  p.contents {
+    margin: 10px;
+  }
 }
 </style>
