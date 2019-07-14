39cf2d95ad2ba1b26215e04a0f5aee88d8420681
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-07-14 15:23:54 +0900

    [ADD]payButton作成

---
+++ components/buttons/payButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button class="paymentButton">
+      クレジットカード
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+  appearance: none;
+}
+.paymentButton {
+  font-size: 40px;
+  height: 85px;
+  width: 391px;
+  color: #0d0d36;
+  background: #ffffff;
+  border: 1px solid #707070;
+  border-radius: 60px;
+}
+</style>
