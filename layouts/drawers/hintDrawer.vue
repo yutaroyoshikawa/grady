947a296d303e454705d5f08e9319c8a116bee534
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-07-18 18:22:18 +0900

    [FIX]hintDrawer修正

---
+++ layouts/drawers/hintDrawer.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="wrap">
+      <div class="close-button-wrapper">
+        <close-button />
+      </div>
+      <div class="main-wrapper">
+        <div class="contents">
+          <h2 class="contents-title">ヒント</h2>
+          <div
+            class="hints"
+            v-for="(hint, index) in hints"
+            :key="'hint' + index"
+          >
+            <hint class="hint" :content="hint" :number="index + 1" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CloseButton from '~/components/buttons/closeButton.vue'
+import Hint from '~/components/texts/hint.vue'
+
+export default Vue.extend({
+  components: {
+    'close-button': CloseButton,
+    hint: Hint
+  },
+  props: {
+    hints: {
+      value: Array,
+      default: ['', '', '']
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  background-color: #3f6060;
+  width: 90vw;
+  height: 100vh;
+
+  .close-button-wrapper {
+    width: 60px;
+    height: 60px;
+    position: absolute;
+    transform: translate(10px, 10px);
+  }
+
+  .main-wrapper {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .contents {
+      width: 237.36px;
+      height: 356.39px;
+      transform: translateY(-33px);
+
+      .contents-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 31px;
+        color: #ffffff;
+        height: 33px;
+        width: 100%;
+        margin-bottom: 75px;
+      }
+
+      .hints {
+        color: #ffffff;
+
+        .hint {
+          margin-bottom: 70px;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/hintDummy.vue
+++ pages/hintDummy.vue
@@ -1,122 +1,37 @@
 <template>
   <div class="wrapper">
-    <div class="hint_menu">
-      <div class="blank_top"></div>
-      <div class="close_button-wrapper">
-        <close-button />
-      </div>
-      <div class="main-wrapper">
-        <main class="contents">
-          <div class="contents-title">ヒント</div>
-          <div class="hints" v-for="(count, index) in demo" :key="index">
-            <hint
-              class="hint_1"
-              :content="count.content"
-              :number="count.number"
-            />
-          </div>
-        </main>
-      </div>
+    <div>
+      <hint-drawer :hints="demo" />
     </div>
   </div>
 </template>
-<script>
+
+<script lang="ts">
 import Vue from 'vue'
-import CloseButton from '../components/buttons/closeButton'
-import Hint from '../components/texts/hint'
+import HintDrawer from '~/layouts/drawers/hintDrawer.vue'
 
 export default Vue.extend({
-  components: { 'close-button': CloseButton, hint: Hint },
-  props: {
-    hints: Array
+  components: {
+    'hint-drawer': HintDrawer
   },
   data() {
     return {
-      demo: [
-        { content: 'hoge', number: 1 },
-        { content: '', number: 2 },
-        { content: '', number: 3 }
-      ]
+      demo: ['hoge', '', '']
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-
 .wrapper {
   background-color: #0d0d36;
-  width: 320px;
-  height: 568px;
+  width: 100vw;
+  height: 100vh;
+
+  div {
+    position: fixed;
+    top: 0;
+    right: 0;
+  }
 }
-
-.hint_menu {
-  margin: 0 0 0 auto;
-  background-color: #3f6060;
-  width: 283px;
-  height: 568px;
-}
-
-/*  blank */
-.blank_top {
-  /*background-color: lightblue;*/
-  height: 13px;
-  width: 100%;
-}
-
-/*close button wrapper*/
-.close_button-wrapper {
-  /*background-color: lightcoral;*/
-  padding-left: 6px;
-  height: 35px;
-  width: 100%;
-}
-.main-wrapper {
-  /*background-color: lightcyan;*/
-  height: 391px;
-  width: 100%;
-
-  /*  中央 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* contents */
-.contents {
-  /*background-color: lightgreen;*/
-  width: 237.36px;
-  height: 356.39px;
-
-  font-family: 'TsukuBRdGothic-Regular', sans-serif;
-}
-
-.contents-title {
-  /*中央揃え*/
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  font-size: 31px;
-  color: #ffffff;
-  height: 33px;
-  width: 100%;
-  /*background-color: lightgray;*/
-
-  /* margin */
-  margin-bottom: 75px;
-}
-
-.hints {
-  color: #ffffff;
-}
-
-.hint_1 {
-  margin-bottom: 70px;
-}
-/*  contents end */
 </style>
