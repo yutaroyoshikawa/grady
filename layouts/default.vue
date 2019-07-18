58c46b1c0f50b09d9658fc90326e105547dde20b
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-07-18 17:30:50 +0900

    [FIX]ロゴだけに修正

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="wrap">
+      <div class="logo-wrap">
+        <logo />
+      </div>
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Logo from '~/components/marks/logo.vue'
+
+export default Vue.extend({
+  components: {
+    logo: Logo
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  .logo-wrap {
+    width: 76px;
+    height: 76px;
+    position: absolute;
+    top: 60px;
+    left: 100px;
+  }
+}
+</style>
