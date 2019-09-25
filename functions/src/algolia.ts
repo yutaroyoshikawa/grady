683dda3274c557849ac8c79c1796b086446c75a9
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-09-25 20:25:18 +0900

    [Fix] algolia関数の修正

--- functions/src/algolia.ts
+++ functions/src/algolia.ts
@@ -1,55 +1,29 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
 
 import * as algoliasearch from 'algoliasearch'
 
 const algolia = algoliasearch(
   functions.config().algolia.app_id as string,
-  functions.config().algolia.api_key as string,
+  functions.config().algolia.api_key as string
 ).initIndex('movies')
 
-// interface IMovie {
-//   // id: number
-//   objectID: number
-//   castName: undefined | string[]
-//   cover: string | null
-//   coverBack: string |null
-//   genre: number[]
-//   releaseDate: string
-//   story: string
-//   subtitle: boolean
-//   title: string
-//   staff: undefined | string[]
-//   // isScreening: boolean
-// }
-
-export const　addMovies = async (movies: any) => {
+export const addMovies = async (movies: any) => {
   await algolia
     .addObjects(movies)
-    .then(() => console.log('algolia追加完了'))
-    .catch(e => e)
-}
-
-export const changeInfo = (movieInfo: any) => {
-  algolia
-    .saveObject(movieInfo)
-    .then(() => 'ok')
+    .then(() => 'algolia追加完了')
     .catch(e => e)
 }
 
 export const updateMovies = (movieInfo: any) => {
-  console.log('algolia更新完了')
   algolia
     .partialUpdateObject(movieInfo)
     .then(() => 'ok')
     .catch(e => e)
 }
 
-
 export const updateAlgolia = (snapshot: any) => {
   const beforeData = snapshot.before.data() as FirebaseFirestore.DocumentData
   const afterData = snapshot.after.data() as FirebaseFirestore.DocumentData
-
-  console.log(beforeData.objectID)
 
   const data = {
     objectID: beforeData.objectID,
@@ -59,7 +33,6 @@ export const updateAlgolia = (snapshot: any) => {
   // isScreeningが変更されていたらalgoliaを更新する
   if (beforeData.isScreening !== afterData.isScreening) {
     updateMovies(data)
-    console.log('algolia更新成功')
   }
 
   return 0
