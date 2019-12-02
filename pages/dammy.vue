effd82a7c472a907823ecf1b258ad7e533ef11c5
Miccili <wot.dannbo@gmail.com>
2019-12-02 12:29:02 +0900

    LoadingMarkを確認できるようにマウント

--- pages/dammy.vue
+++ pages/dammy.vue
@@ -3,16 +3,21 @@
     <div class="wrap">
       <go-to-watch-drawer />
     </div>
+    <div>
+      <loading-mark />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import GoToWatchDrawer from '../layouts/drawers/goToWatchDrawer.vue'
+import LoadingMark from '../components/marks/loadingMark.vue'
 
 export default Vue.extend({
   components: {
-    'go-to-watch-drawer': GoToWatchDrawer
+    'go-to-watch-drawer': GoToWatchDrawer,
+    'loading-mark': LoadingMark
   }
 })
 </script>
