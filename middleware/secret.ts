3bd1e32fa6551fd3286a10092d362234ec213bf7
Cherry <ex00ia2432@gmail.com>
2019-12-04 10:03:58 +0900

    [clean]

--- middleware/secret.ts
+++ middleware/secret.ts
@@ -1,12 +1,16 @@
-export default (
-function (route: { genre: string; params: any; }) {
-    // log出力
-    const check = route.params.genre
-    if (check == 'Action' || check == 'Suspense' || check == 'Science Fiction' ||check == 'Horror' || check == 'Comedy' || check == 'Romance') {
-      return
-    } else if (check == null) {
-      return
-    } else {
-      window.alert('不正アクセスです')
-    }
-}) 
+export default (function(route: { genre: string; params: any }) {
+  // log出力
+  const check = route.params.genre
+  if (
+    check === 'Action' ||
+    check === 'Suspense' ||
+    check === 'Science Fiction' ||
+    check === 'Horror' ||
+    check === 'Comedy' ||
+    check === 'Romance'
+  ) {
+  } else if (check == null) {
+  } else {
+    window.alert('不正アクセスです')
+  }
+})
