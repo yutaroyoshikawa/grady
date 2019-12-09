883b5bbb42c22fe19f27c530ee506bc921264880
Miccili <wot.dannbo@gmail.com>
2019-12-09 11:09:17 +0900

    [FIX]baseにあったStyleをコンポーネントに移した

--- components/marks/loadingMark.vue
+++ components/marks/loadingMark.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="wrap">
-    <div class="lines">
-      <div class="line1"></div>
-      <div class="line2"></div>
-      <div class="line3"></div>
-      <div class="line4"></div>
+    <div class="loading-wrap">
+      <div class="lines">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+        <div class="line4"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,60 +18,71 @@ export default Vue.extend({})
 
 <style lang="scss" scoped>
 .wrap {
-  width: 240px;
-  height: 260px;
-  z-index: 999;
-  border-radius: 60px;
+  width: 100vw;
+  height: 100vh;
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-content: center;
-  background-color: #222222;
+  flex-direction: column;
+  align-items: center;
+  z-index: 666;
+  background-color: rgba(000, 000, 000, 0.8);
 
-  .lines {
-    height: 240px;
+  .loading-wrap {
+    width: 240px;
+    height: 260px;
+    z-index: 999;
+    border-radius: 60px;
     display: flex;
-    justify-content: space-evenly;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    background-color: #222222;
 
-    .line1 {
-      width: 15%;
-      height: 100%;
-      border-radius: 20px;
-      animation-name: loding;
-      animation-duration: 3s;
-      animation-iteration-count: infinite;
-      background-color: #ffffff;
-      animation-delay: -0s;
-    }
-    .line2 {
-      width: 15%;
-      height: 100%;
-      border-radius: 20px;
-      animation-name: loding;
-      animation-duration: 3s;
-      animation-iteration-count: infinite;
-      animation-delay: -0.2s;
-      background-color: #ffffff;
-    }
-    .line3 {
-      width: 15%;
-      height: 100%;
-      border-radius: 20px;
-      animation-name: loding;
-      animation-duration: 3s;
-      animation-iteration-count: infinite;
-      animation-delay: -0.4s;
-      background-color: #ffffff;
-    }
-    .line4 {
-      width: 15%;
-      height: 100%;
-      border-radius: 20px;
-      animation-name: loding;
-      animation-duration: 3s;
-      animation-iteration-count: infinite;
-      animation-delay: -0.6s;
-      background-color: #ffffff;
+    .lines {
+      height: 240px;
+      display: flex;
+      justify-content: space-evenly;
+
+      .line1 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loding;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        background-color: #ffffff;
+        animation-delay: -0s;
+      }
+      .line2 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loding;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-delay: -0.2s;
+        background-color: #ffffff;
+      }
+      .line3 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loding;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-delay: -0.4s;
+        background-color: #ffffff;
+      }
+      .line4 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loding;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-delay: -0.6s;
+        background-color: #ffffff;
+      }
     }
   }
 }
