014fa303be4d483d7869c99663aae335ee662d79
Miccili <wot.dannbo@gmail.com>
2019-12-17 17:39:17 +0900

    [ADD]loadingMarkを追加

---
+++ components/marks/loadingMark.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="wrap">
+    <div class="loading-wrap">
+      <div class="lines">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+        <div class="line4"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  z-index: 666;
+  background-color: rgba(000, 000, 000, 0.8);
+
+  .loading-wrap {
+    width: 240px;
+    height: 260px;
+    z-index: 999;
+    border-radius: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    background-color: #222222;
+
+    .lines {
+      height: 240px;
+      display: flex;
+      justify-content: space-evenly;
+
+      .line1 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loading;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        background-color: #ffffff;
+        animation-delay: -0s;
+      }
+      .line2 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loading;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-delay: -0.2s;
+        background-color: #ffffff;
+      }
+      .line3 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loading;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-delay: -0.4s;
+        background-color: #ffffff;
+      }
+      .line4 {
+        width: 15%;
+        height: 100%;
+        border-radius: 20px;
+        animation-name: loading;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-delay: -0.6s;
+        background-color: #ffffff;
+      }
+    }
+  }
+}
+@keyframes loading {
+  0% {
+    height: 0%;
+  }
+  50% {
+    height: 100%;
+  }
+  100% {
+    height: 0%;
+  }
+}
+</style>
