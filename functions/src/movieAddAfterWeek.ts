00b3492f28a233fd382f3d9263a684b1b47b68e2
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-12-18 11:42:26 +0900

    [Fix] コメント削除

--- functions/src/movieAddAfterWeek.ts
+++ functions/src/movieAddAfterWeek.ts
@@ -171,20 +171,6 @@ const secretMovie = async () => {
   const values = Object.values(secretMovieData).filter(value => value)
   return values.map((value: any) => value.genre)
 }
-//
-// const nowPlayingMovieAdd = (theaterName: theaterName, dateTime: string, movieId: string, theaterNumber: number) => {
-//   const data = {
-//     movieId,
-//     sheet: [],
-//   }
-//   db.collection('theaterInfo')
-//     .doc(theaterName)
-//     .collection(dateTime)
-//     .doc(`theater${theaterNumber}`)
-//     .set(data)
-//     .then(() => 'ok')
-//     .catch(e => console.log(e))
-// }
 
 interface Interface {
   theaterName: TheaterName
@@ -200,10 +186,8 @@ const movieAdd = (data: Interface) => {
     nowPlayingSecretMovieGenre,
     nowPlayingMovieId
   } = data
-  // let theaterNumber = 1
 
   const dataAdd = async (set: object) => {
-    // console.log(number);
     await db
       .collection('theaterInfo')
       .doc(theaterName)
@@ -215,24 +199,19 @@ const movieAdd = (data: Interface) => {
   }
 
   nowPlayingSecretMovieGenre.forEach(async (id: string) => {
-    // console.log(theaterNumber);
     const set = {
       genre: id,
       sheet
     }
     await dataAdd(set)
-    // theaterNumber += 1
   })
 
   nowPlayingMovieId.forEach(async (id: string) => {
-    // console.log(theaterNumber);
-
     const set = {
       movieId: id,
       sheet
     }
     await dataAdd(set)
-    // theaterNumber += 1
   })
 }
 
